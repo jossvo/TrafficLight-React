5b5e41f9b6f7d09b8f02197fed0ed8fbf99378fb
jossvo <jossvo26@outlook.com>
2022-12-16 18:52:15 +0000

    Final versión without setInterval()

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,51 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [color,setColor]=useState("red")
+	const [lightState,setLightState]=useState("stop")
+	const [lights,setLights]=useState(["red","yellow","green"])
+
+	const changeColor = ()=>{
+		if(color==="red")setColor("yellow")
+		else if(color==="yellow")setColor("green")
+		else if(color==="green"&&lights.length===3||color==="purple")setColor("red")
+		else setColor("purple")
+	};
+	const addPurple = ()=>{if(lights.includes("purple")){
+			setColor("red")
+			setLights(["red","yellow","green"])}
+			else setLights(lights.concat(["purple"]))
+	};
+
+	//setInterval(changeColor,1000);
+
+	/*
+	var globalTimer=""
+	const startTimer = ()=>{
+		if(lightState==="stop")globalTimer=setInterval(changeColor,2000)
+		else{
+			clearInterval(globalTimer);
+			setLightState("stop")
+		}
+	};
+	*/
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="main">
+			<div className="post"></div>
+			<div className="stoplight">
+				{lights.map((light,index)=>{
+					return <div className={
+						"light "+
+						light+
+						(color===light?" on":"")
+					} key={index}></div>
+				})
+				}
+				<button onClick={changeColor}>{lightState==="running"?"stop":"change"}</button>
+				<button id="purple" onClick={addPurple}>{lights.includes("purple")?"remove":"add"}</button>
+			</div>
 		</div>
 	);
 };
