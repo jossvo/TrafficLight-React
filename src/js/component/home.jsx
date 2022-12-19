731b1741b4f804ab0cb7486b190ff0e0b5f179c2
jossvo <jossvo26@outlook.com>
2022-12-19 16:12:21 +0000

    Final version w/ setInterval

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,53 +1,63 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 //create your first component
 const Home = () => {
-	const [color,setColor]=useState("red")
-	const [lightState,setLightState]=useState("stop")
-	const [lights,setLights]=useState(["red","yellow","green"])
+  const [color, setColor] = useState("red");
+  const [lightState, setLightState] = useState("running");
+  const [lights, setLights] = useState(["red", "yellow", "green"]);
 
-	const changeColor = ()=>{
-		if(color==="red")setColor("yellow")
-		else if(color==="yellow")setColor("green")
-		else if(color==="green"&&lights.length===3||color==="purple")setColor("red")
-		else setColor("purple")
-	};
-	const addPurple = ()=>{if(lights.includes("purple")){
-			setColor("red")
-			setLights(["red","yellow","green"])}
-			else setLights(lights.concat(["purple"]))
-	};
+  const changeColor = () => {
+    if (lightState === "running") {
+      if (color === "red") setColor("yellow");
+      else if (color === "yellow") setColor("green");
+      else if ((color === "green" && lights.length === 3) || color === "purple")
+        setColor("red");
+      else setColor("purple");
+    }
+  };
+  const addPurple = () => {
+    if (lights.includes("purple")) {
+      setColor("red");
+      setLights(["red", "yellow", "green"]);
+    } else setLights(lights.concat(["purple"]));
+  };
 
-	//setInterval(changeColor,1000);
+  useEffect(() => {
+    let intervalid = setInterval(changeColor, 1000);
+    return () => {
+      clearInterval(intervalid);
+    };
+  }, [color,lightState]);
 
-	/*
-	var globalTimer=""
-	const startTimer = ()=>{
-		if(lightState==="stop")globalTimer=setInterval(changeColor,2000)
-		else{
-			clearInterval(globalTimer);
-			setLightState("stop")
-		}
-	};
-	*/
-
-	return (
-		<div className="main">
-			<div className="post"></div>
-			<div className="stoplight">
-				{lights.map((light,index)=>{
-					return <div className={
-						"light "+
-						light+
-						(color===light?" on":"")
-					} key={index}></div>
-				})
-				}
-				<button onClick={changeColor}>{lightState==="running"?"stop":"change"}</button>
-				<button id="purple" onClick={addPurple}>{lights.includes("purple")?"remove":"add"}</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="main">
+      <div className="post"></div>
+      <div className="stoplight">
+        {lights.map((light, index) => {
+          return (
+            <div
+              className={"light " + light + (color === light ? " on" : "")}
+              key={index}
+            ></div>
+          );
+        })}
+        <button
+          onClick={() =>
+            lightState === "running"
+              ? setLightState("stopped")
+              : setLightState("running")
+          }
+		  className={lightState === "running" ? "StartButton-on" : "StartButton-off"}
+        >
+          {lightState === "running" ? "stop" : "start"}
+        </button>
+        <button id="purple" onClick={addPurple}
+		className={lights.includes("purple") ? "purpleButton-on" : "purpleButton"}>
+          {lights.includes("purple") ? "remove" : "add"}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
